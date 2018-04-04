346e8b370f647c805ecbea5857d0125f8969ce31
Santiago Peraza <santiago.peraza@abstracta.com.uy>
2018-04-04 08:40:02 -0300

    Agregue menu del main y funcion existeSocio para usar en agregarSocio

--- ejercicio1/main.cpp
+++ ejercicio1/main.cpp
@@ -1,5 +1,7 @@
+
+#include <cstdlib>
 #include <iostream>
-#include <string>
+#include <stdio.h>
 #include <exception>
 #include "DtClase.h"
 #include "DtSocio.h"
@@ -12,45 +14,131 @@
 
 using namespace std;
 
-void agregarSocio(Socio*, string, string);
+struct socios {
+  Socio * socios[MAX_SOCIOS];
+  int tope;
+}
+listaDeSocios;
+
+void agregarSocio(string, string);
+void inicializarSocios();
+bool existeSocio(string);
+void menu();
 
 int main() {
+  inicializarSocios();
 
-    Socio* socios = new Socio[MAX_SOCIOS];
-    //inicializar socios
-    for (int i = 0; i < MAX_SOCIOS; i++) {
-        socios[i].setCI("");
-        socios[i].setNombre("");
+  string nombre;
+  string cedula;
+  int numOper = 0;
+  bool salir = false;
+  char opcion = 'n';
+  do {
+    menu();
+    cin >> numOper;
+    switch (numOper) {
+    case 1:
+      cout << "\n\tAgregar socio\n\n";
+      cout << "Ingrese la CI: ";
+      cin >> cedula;
+      cout << "Ingrese el nombre: ";
+      cin >> nombre;
+      agregarSocio(cedula, nombre);
+      break;
+    case 2:
+      cout << "\n\tAgregar clase\n";
+      break;
+    case 3:
+      cout << "\n\tAgregar inscripcion\n";
+      break;
+    case 4:
+      cout << "\n\tBorrar inscripcion\n";
+      break;
+    case 5:
+      cout << "\n\tObtener informacion de socio por clase\n";
+
+      break;
+    case 6:
+      cout << "\n\tObtener clase\n";
+      break;
+    case 7:
+      cout << "\n\tImprimir clases\n";
+      break;
+    case 8:
+      cout << "\nEsta seguro de que desea salir (s/n)?: ";
+      cin >> opcion;
+      if (opcion == 's' || opcion == 'S') {
+        cout << "Saliendo...\n";
+        salir = true;
+      }
+      break;
+    default:
+      cout << "\nNo ingreso una opcion valida, vuelva a intentarlo...\n";
     }
+    cout << "\n";
+    cout << "Presione Enter Para continuar";
+    fflush(stdin);
+    cin.ignore();
+  } while (!salir);
 
-    agregarSocio(socios, "1", "Matias");
-    cout << socios[0] << endl;
+  Clase** clases = new Clase * [MAX_CLASES]; //arreglo para una clase abstracta.
+  return 0;
+}
 
+void inicializarSocios() {
+  listaDeSocios.tope = 0;
+}
 
-    Clase** clases = new Clase*[MAX_CLASES]; //arreglo para una clase abstracta.
-
-    cout << "Ejercicio 1" << endl;
-    return 0;
+void menu() {
+  cout << "\nProgramacion Avanzada - Laboratorio 1\n\n";
+  cout << "\t\tEjercicio 1 - GIMNASIO\n\n";
+  cout << "Lista de operaciones disponibles:\n";
+  cout << "1)  Agregar nuevo socio\n";
+  cout << "2)  Agregar clase\n";
+  cout << "3)  Agregar inscripcion\n";
+  cout << "4)  Borrar inscripcion\n";
+  cout << "5)  Obtener informacion de socio por clase\n";
+  cout << "6)  Obtener clase\n";
+  cout << "7)  Imprimir clases\n";
+  cout << "8)  Salir\n\n";
+  cout << "Ingrese el numero de la operacion a realizar: ";
 }
 
 //Crea un nuevo socio en el sistema. En caso de ya existir, levanta la excepción
 //std::invalid_argument.
-void agregarSocio(Socio* socios, string ci, string nombre){
-    for (int i = 0; i < MAX_SOCIOS; i++) {
-        if (socios[i].getCI() == ci){
-            throw std::invalid_argument("Ya existe el socio");
-        }
-        if (socios[i].getCI() == ""){
-            socios[i].setCI(ci);
-            socios[i].setNombre(nombre);
-            break;
-        }
+void agregarSocio(string ci, string nombre) {
+  if (listaDeSocios.tope < MAX_SOCIOS) {
+    if (existeSocio(ci) == true) {
+      throw std::invalid_argument("Ya existe el socio");
+    } else {
+      Socio* nuevoSocio = new Socio();
+      nuevoSocio->setCI(ci);
+      nuevoSocio->setNombre(nombre);
+      listaDeSocios.socios[listaDeSocios.tope  + 1] = nuevoSocio;
+      cout << "\nSe agrego con exito.\n";
     }
+  } else {
+    cout << "\nNo se puede agregar, se alcanzo el maximo numero de socios.\n";
+  }
+}
+
+bool existeSocio(string ci) {
+  int indice = 1;
+  bool existe = false;
+  while (existe == false && indice <= listaDeSocios.tope) {
+    if (listaDeSocios.socios[indice]-> getCI() == ci) {
+      existe = true;
+    } else {
+      cout << "no existe";
+      indice++;
+    }
+  }
+  return existe;
 }
 
 //Crea una nueva clase en el sistema. En caso de ya existir, levanta una excepción
 //std::invalid_argument.
-void agregarClase(DtClase& clase){
+void agregarClase(DtClase & clase) {
 
 }
 
@@ -58,23 +146,23 @@ void agregarClase(DtClase& clase){
 //la clase existan, de lo contrario se levanta una excepción std::invalid_argument. Si ya existe una
 //inscripción de ese usuario para esa clase, o si el cupo de esa clase ya fue alcanzado, también se
 //levanta una excepción std::invalid_argument.
-void agregarInscripcion(string ciSocio, int idClase, Fecha fecha){
+void agregarInscripcion(string ciSocio, int idClase, Fecha fecha) {
 
 }
 
 //Borra la inscripción de un socio a una clase. Si no existe una inscripción de ese usuario para esa
 //clase, se levanta una excepción std::invalid_argument.
-void borrarInscripcion(string ciSocio, int idClase){
+void borrarInscripcion(string ciSocio, int idClase) {
 
 }
 
 //Retorna un arreglo con los socios que están inscriptos a determinada clase. El largo del arreglo de
 //socios está dado por el parámetro cantSocios.
-DtSocio** obtenerInfoSociosPorClase (int idClase, int& cantSocios){
+DtSocio** obtenerInfoSociosPorClase(int idClase, int & cantSocios) {
 
 }
 
 //Retorna la información de la clase identificada por idClase.
-DtClase& obtenerClase(int idClase){
+DtClase & obtenerClase(int idClase) {
 
 }
--- ejercicio1/src/DtEntrenamiento.cpp
+++ ejercicio1/src/DtEntrenamiento.cpp
@@ -16,33 +16,33 @@ bool DtEntrenamiento::getEnRambla(){
 }
 
 //error al utilizar el puntero
-ostream& operator<<(ostream &os, DtEntrenamiento& dtEntrenamiento) {
-    string turno;
-    if(dtEntrenamiento.getTurno() == Turno::Manana){
-        turno = "Mañana";
-    }
-    else if (dtEntrenamiento.getTurno() == Turno::Tarde){
-        turno = "Tarde";
-    }
-    else if (dtEntrenamiento.getTurno() == Turno::Noche){
-        turno = "Noche";
-    }
-    else{
-        turno = "";
-    }
-
-    string stringEnRambla;
-    if (true){
-        stringEnRambla = "Sí";
-    }
-    else{
-        stringEnRambla = "No";
-    }
-
-    os <<
-        "ID: " << dtEntrenamiento.getId() <<"\n" <<
-        "Nombre: " << dtEntrenamiento.getNombre() << "\n"<<
-        "Turno: " << turno << "\n" <<
-        "En rambla: " << dtEntrenamiento << "\n";
-    return os;
-}
+//ostream& operator<<(ostream &os, DtEntrenamiento& dtEntrenamiento) {
+//    string turno;
+//    if(dtEntrenamiento.getTurno() == Turno::Manana){
+//        turno = "Mañana";
+//    }
+//    else if (dtEntrenamiento.getTurno() == Turno::Tarde){
+//        turno = "Tarde";
+//    }
+//    else if (dtEntrenamiento.getTurno() == Turno::Noche){
+//        turno = "Noche";
+//    }
+//    else{
+//        turno = "";
+//    }
+//
+//    string stringEnRambla;
+//    if (true){
+//        stringEnRambla = "Sí";
+//    }
+//    else{
+//        stringEnRambla = "No";
+//    }
+//
+//    os <<
+//        "ID: " << dtEntrenamiento.getId() <<"\n" <<
+//        "Nombre: " << dtEntrenamiento.getNombre() << "\n"<<
+//        "Turno: " << turno << "\n" <<
+//        "En rambla: " << dtEntrenamiento << "\n";
+//    return os;
+//}
